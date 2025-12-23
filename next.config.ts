6ba7f45de6c5635fce205f4ad0b730306e4bc9be
t1mzzz <timothykoei@gmail.com>
2025-12-23 17:06:08 +0800

    Add trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  images: {
-    unoptimized: true,
-  },
+  trailingSlash: true, // optional, generates /about/index.html
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
